a547cbd9d1d3471b3bcf7527d506140f4d85f30d
jisu0811 <jisu08yoo@gmail.com>
2025-08-12 04:01:52 +0900

    Header, Footer 컴포넌트 작성

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./app.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById('app'))
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
